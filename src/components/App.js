08be68f42c9eb02030645d013923d11e192c2718
Piyush Jain <piyushjain5350@gmail.com>
2024-01-30 23:12:36 +0530

    1 soln

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [val,setVal]=useState('');
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text" value={val} onChange={(e)=>setVal(e.target.value)} />
+        {
+          (val.length>0)?(<p>Hello {val}!</p>):(
+            <p></p>
+          )
+        }
     </div>
   )
 }
