c5dafaec5099ece6869ec57875d83fdbe3b21373
Piyush Jain <piyushjain5350@gmail.com>
2024-01-30 23:18:54 +0530

    2 soln

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,6 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <p>Enter your name:</p>
         <input type="text" value={val} onChange={(e)=>setVal(e.target.value)} />
         {
           (val.length>0)?(<p>Hello {val}!</p>):(
